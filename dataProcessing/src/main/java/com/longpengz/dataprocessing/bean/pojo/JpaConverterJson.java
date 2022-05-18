8a80ab9fd24ad9ed509758abee800171f6c40314
longpengz <longpengz@foxmail.com>
2022-05-18 15:40:47 +0800

    jpa json null检查

--- dataProcessing/src/main/java/com/longpengz/dataprocessing/bean/pojo/JpaConverterJson.java
+++ dataProcessing/src/main/java/com/longpengz/dataprocessing/bean/pojo/JpaConverterJson.java
@@ -2,6 +2,8 @@ package com.longpengz.dataprocessing.bean.pojo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.AttributeConverter;
 import java.io.IOException;
@@ -13,18 +15,26 @@ public class JpaConverterJson implements AttributeConverter<Object, String> , Se
 
     @Override
     public String convertToDatabaseColumn(Object meta) {
+        if(ObjectUtils.isEmpty(meta)){
+            return null;
+        }
         try {
             return objectMapper.writeValueAsString(meta);
         } catch (JsonProcessingException ex) {
+            ex.printStackTrace();
             return null;
         }
     }
 
     @Override
     public Object convertToEntityAttribute(String dbData) {
+        if(!StringUtils.hasLength(dbData)){
+            return null;
+        }
         try {
             return objectMapper.readValue(dbData, Object.class);
         } catch (IOException ex) {
+            ex.printStackTrace();
             return null;
         }
     }
