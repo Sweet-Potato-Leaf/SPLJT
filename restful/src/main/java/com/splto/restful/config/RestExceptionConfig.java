cafbf2edccc7876bf41245b69615ad341f3c537f
longpengZ <longpengz@foxmail.com>
2023-04-02 12:13:56 +0800

    perf:异常信息优化

--- restful/src/main/java/com/splto/restful/config/RestExceptionConfig.java
+++ restful/src/main/java/com/splto/restful/config/RestExceptionConfig.java
@@ -5,10 +5,12 @@ import com.splto.restful.model.API;
 import com.splto.restful.model.APIException;
 import javax.annotation.Resource;
 
+import com.splto.utils.ExceptionUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.NestedExceptionUtils;
 import org.springframework.util.ObjectUtils;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
@@ -22,6 +24,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Arrays;
 
 
 @Slf4j
@@ -35,7 +38,7 @@ public class RestExceptionConfig {
     public API<Object> illegalParamsExceptionHandler(MethodArgumentNotValidException ex) {
         BindingResult bindingResult = ex.getBindingResult();
         FieldError fieldError = bindingResult.getFieldError();
-        log.error(ex.getMessage());
+        log.error(ExceptionUtil.toStackTrace(ex));
         return API.e(400, !ObjectUtils.isEmpty(fieldError)? fieldError.getDefaultMessage():"");
 
     }
@@ -43,13 +46,13 @@ public class RestExceptionConfig {
     @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
     public API<Object> methodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
         String error = String.format("The parameter '%s' should be of type '%s'", ex.getName(), !ObjectUtils.isEmpty(ex.getRequiredType())? ex.getRequiredType().getSimpleName():"");
-        log.error(ex.getMessage());
+        log.error(ExceptionUtil.toStackTrace(ex));
         return API.e(400, error);
     }
 
     @ExceptionHandler(value = {NoHandlerFoundException.class})
     public API<Object> noHandlerFoundException(Exception ex) {
-        log.error(ex.getMessage());
+        log.error(ExceptionUtil.toStackTrace(ex));
         return API.e(404, "Resource Not Found");
     }
 
@@ -59,30 +62,31 @@ public class RestExceptionConfig {
         builder.append(ex.getContentType());
         builder.append(" media type is not supported. Supported media types are ");
         ex.getSupportedMediaTypes().forEach(t -> builder.append(t).append(","));
-        log.error(ex.getMessage());
+        log.error(ExceptionUtil.toStackTrace(ex));
         return API.e(415, builder.toString());
     }
 
     @ExceptionHandler(value = {HttpRequestMethodNotSupportedException.class})
     public API<Object> httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
-        log.error(ex.getMessage());
+        log.error(ExceptionUtil.toStackTrace(ex));
         return API.e(405, ex.getMessage());
     }
 
     @ExceptionHandler(value = {APIException.class})
     public API<Object> APIException(APIException ex) {
-        log.error(ex.getMessage());
+        log.error(ExceptionUtil.toStackTrace(ex));
         return API.e(ex.getApiError().getCode(), ex.getApiError().getMsg());
     }
 
     @ExceptionHandler(value = {IllegalArgumentException.class})
     public API<Object> APIException(IllegalArgumentException ex) {
-        ex.printStackTrace();
+        log.error(ExceptionUtil.toStackTrace(ex));
         return API.e(406, ex.getMessage());
     }
 
     @ExceptionHandler(BindException.class)
     public API<Object> handleConstraintViolationException(BindException e) {
+        log.error(ExceptionUtil.toStackTrace(e));
         return API.e(400, (ObjectUtils.isEmpty(e.getBindingResult().getFieldError()) ? "" : e.getBindingResult().getFieldError().getDefaultMessage()));
     }
 
@@ -93,6 +97,7 @@ public class RestExceptionConfig {
             PrintWriter printWriter = new PrintWriter( writer );
             ex.printStackTrace(printWriter);
         }
+        log.error(ExceptionUtil.toStackTrace(ex));
         return API.e(500,"服务器错误");
     }
 }
